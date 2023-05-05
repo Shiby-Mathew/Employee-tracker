0cb685c4f48b83123263b39f4c7541820f3214c2
Shiby-Mathew <shiby.ajay@gmail.com>
2023-05-05 14:11:19 +1000

    install console.table and working on the functionality, view all department, all roles all employees working

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-//run npm init -y
-//run npm i inquirer
-//create a class for index.js
-
 const inquirer = require("inquirer");
-
+const cTable = require("console.table");
 const mysql = require("mysql2");
 
+//TODO Change password in .env file
 // Connect to database
 const db = mysql.createConnection(
   {
     host: "localhost",
-    // MySQL username,
     user: "root",
-    // TODO: Add MySQL password here
     password: "root",
     database: "employee_db",
   },
@@ -29,29 +24,38 @@ const main = () => {
         message: "What would you like to do?",
         name: "choices",
         choices: [
-          "View all departments",
-          "View all roles",
-          "View all employees",
-          "Add a department",
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add Department",
+          "Add Role",
+          "Add Employee",
         ],
       },
     ])
     .then((data) => {
-      console.log(data);
       const { choices } = data;
 
-      if (choices === "View all departments") {
-        console.log("inside choices");
+      if (choices === "View All Departments") {
         showDepartment();
       }
 
-      if (choices === "View all roles") {
-        console.log("inside choices");
+      if (choices === "View All Roles") {
         showRoles();
       }
-      if (choices === "View all employees") {
-        console.log("inside choices");
+      if (choices === "View All Employees") {
         showAllEmployees();
+      }
+      if (choices === "Add Department") {
+        addDepartment();
+      }
+      if (choices === "Add Role") {
+        console.log("inside choices");
+        addRole();
+      }
+      if (choices === "Add Employee") {
+        console.log("inside choices");
+        addEmployee();
       }
     });
 }; //main
@@ -71,7 +75,7 @@ const showDepartment = () => {
 const showRoles = () => {
   console.log("All Roles");
   const sql =
-    "SELECT role.id,role.title,department.dep_name AS department,role.salary FROM role JOIN department ON department.id = role.department_id ORDER BY role.id ";
+    "SELECT role.id,role.title,department.dep_name AS department,role.salary FROM role LEFT JOIN department ON department.id = role.department_id ORDER BY role.id ";
   db.query(sql, (err, rows) => {
     if (err) {
       console.log("ERRor");
@@ -83,7 +87,8 @@ const showRoles = () => {
 };
 const showAllEmployees = () => {
   console.log("All Employees");
-  const sql ="SELECT employee.id,employee.first_name,employee.last_name,role.title,department.dep_name AS department,role.salary,manager.first_name AS manager FROM employee LEFT join role ON role.id= employee.role_id LEFT join department ON department.id= role.department_id LEFT JOIN employee AS manager ON employee.manager_id = manager.id";
+  const sql =
+    "SELECT employee.id,employee.first_name,employee.last_name,role.title,department.dep_name AS department,role.salary,manager.first_name AS manager FROM employee LEFT join role ON role.id= employee.role_id LEFT join department ON department.id = role.department_id LEFT JOIN employee AS manager ON employee.manager_id = manager.id";
   db.query(sql, (err, rows) => {
     if (err) {
       console.log("ERRor");
@@ -93,5 +98,85 @@ const showAllEmployees = () => {
     main();
   });
 };
+
+const addDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the name of the department?",
+        name: "depName",
+      },
+    ])
+    .then((res) => {
+      console.log(res);
+      const sql = "INSERT INTO department(dep_name) VALUES(?)";
+      db.query(sql, res.depName, (err, rows) => {
+        if (err) {
+          console.log("ERRor");
+          return;
+        }
+        console.log("Added " + res.depName + "to the database");
+        main();
+      });
+    });
+};
+
+//NEED to complete
+const addRole = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the name of the role?",
+      name: "roleName",
+    },
+    {
+      type: "input",
+      message: "What is the salary of the role?",
+      name: "salary",
+    },
+
+    //TODO how to get all department from database
+    {
+      type: "list",
+      message: "Which department does the role belong to?",
+      name: "department",
+      choices: ["list"],
+    },
+  ]);
+  main();
+};
+
+//TODO need to get values
+const addEmployee = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the employee's first name?",
+      name: "firstName",
+    },
+    {
+      type: "input",
+      message: "What is the employee's last name?",
+      name: "lastName",
+    },
+    //TODO need to get values
+    {
+      type: "list",
+      message: "What is the employee's role?",
+      name: "empRole",
+      choices: ["list"],
+    },
+    {
+      type: "list",
+      message: "Who is the employee's manager?",
+      name: "empManager",
+      choices: ["list"],
+    },
+  ]);
+  main();
+};
+//TODO UPDATE an empoyee's role
+//DELETE an employee
 
 main();
