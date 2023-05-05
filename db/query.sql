49f49f7a6fe008caff5619911e6d6ae362ee4e0a
Shiby-Mathew <shiby.ajay@gmail.com>
2023-05-05 12:58:38 +1000

    added functionality but not working still working on it

--- db/query.sql
+++ db/query.sql
@@ -1,24 +1,38 @@
 -- 1. View all departments
--- SELECT * FROM department;
+-- SELECT id,dep_name AS department FROM department;
 
 -- -- 2. View all roles
--- select role.id,role.title,department.dep_name AS department,role.salary FROM role
---  JOIN department ON department.id = role.department_id order by role.id ;
+-- SELECT role.id,role.title,department.dep_name AS department,role.salary FROM role
+--  JOIN department ON department.id = role.department_id ORDER BY role.id ;
 
 -- -- 3. View all employees
 
-select employee.id,employee.first_name,employee.last_name,role.title,department.dep_name AS department,role.salary,manager.first_name As manager
-from employee
-LEFT join role On role.id= employee.role_id 
-LEFT join department On department.id= role.department_id 
-left join employee AS manager On employee.manager_id = manager.id
-;
+-- SELECT employee.id,employee.first_name,employee.last_name,role.title,department.dep_name AS department,role.salary,manager.first_name AS manager
+-- FROM employee
+-- LEFT join role ON role.id= employee.role_id 
+-- LEFT join department ON department.id= role.department_id 
+-- left join employee AS manager ON employee.manager_id = manager.id
+-- ;
+
+-- 4. Add a department
+    INSERT INTO department(dep_name) VALUES(?);
 
 
 
--- add a department
--- add a role
+--  enter dep_name
+
+
+-- 5. add a role
+
+-- name,salary,department for role
+ INSERT INTO role(title,salary,department_id) VALUES(?,?,?);
+
 -- add an employee
+-- first name ,last name role,manager
+
 -- update an employee role
+-- update new role
+
+
 --  use employee_db;
 
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-//import inquirer
 //run npm init -y
 //run npm i inquirer
 //create a class for index.js
+
+const inquirer = require("inquirer");
+
+const mysql = require("mysql2");
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // TODO: Add MySQL password here
+    password: "root",
+    database: "employee_db",
+  },
+  console.log(`Connected to the employee_db database.`)
+);
 
 //main function
 const main = () => {
@@ -9,26 +25,73 @@ const main = () => {
   inquirer
     .prompt([
       {
-        type: "input",
-        message: "What is the title of this application ?",
-        name: "name",
-      },
-
-      {
         type: "list",
-        message: "Select which kind of license for this application",
-        name: "license",
+        message: "What would you like to do?",
+        name: "choices",
         choices: [
-          "None",
-          "Apache License",
-          "MIT License",
-          "Boost Software License",
-          "GNU General Public License",
-          "Mozilla Public Licence",
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
         ],
       },
     ])
     .then((data) => {
       console.log(data);
+      const { choices } = data;
+
+      if (choices === "View all departments") {
+        console.log("inside choices");
+        showDepartment();
+      }
+
+      if (choices === "View all roles") {
+        console.log("inside choices");
+        showRoles();
+      }
+      if (choices === "View all employees") {
+        console.log("inside choices");
+        showAllEmployees();
+      }
     });
+}; //main
+
+const showDepartment = () => {
+  console.log("All departments");
+  const sql = "SELECT id,dep_name AS department FROM department";
+  db.query(sql, (err, rows) => {
+    if (err) {
+      console.log("ERRor");
+      return;
+    }
+    console.table(rows);
+    main();
+  });
 };
+const showRoles = () => {
+  console.log("All Roles");
+  const sql =
+    "SELECT role.id,role.title,department.dep_name AS department,role.salary FROM role JOIN department ON department.id = role.department_id ORDER BY role.id ";
+  db.query(sql, (err, rows) => {
+    if (err) {
+      console.log("ERRor");
+      return;
+    }
+    console.table(rows);
+    main();
+  });
+};
+const showAllEmployees = () => {
+  console.log("All Employees");
+  const sql ="SELECT employee.id,employee.first_name,employee.last_name,role.title,department.dep_name AS department,role.salary,manager.first_name AS manager FROM employee LEFT join role ON role.id= employee.role_id LEFT join department ON department.id= role.department_id LEFT JOIN employee AS manager ON employee.manager_id = manager.id";
+  db.query(sql, (err, rows) => {
+    if (err) {
+      console.log("ERRor");
+      return;
+    }
+    console.table(rows);
+    main();
+  });
+};
+
+main();
